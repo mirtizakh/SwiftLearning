5a05f2d2db095e8bf4b8a21f0c011726eac2d456
Irtiza <Irtizakh@gmail.com>
2021-05-19 11:35:10 +0500

    Variables , Multi-line strings , String interpolation , Constants , Type annotations .

--- Day1/MyPlayground.playground/Contents.swift
+++ Day1/MyPlayground.playground/Contents.swift
@@ -1,3 +1,28 @@
 import UIKit
 
-var str = "Hello, playground"
+// we can store integer value with underscore
+var number = 8_000_000
+
+// multiline string in swift
+var multiLine = """
+This goes in \
+over multiple \
+lines
+"""
+
+
+// String interpolation
+var score = 50
+var str = "My score is \(score) "
+
+// to create constants
+let data = "Audio"
+
+// Type annotation
+var name :String = "Ali"
+
+
+// Summary
+// 1- You make variables using var and constants using let. It’s preferable to use constants as often as possible.
+// 
+
