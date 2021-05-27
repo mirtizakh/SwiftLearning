ec16e65e4891dc061b1fb59b5187731f389130fa
Irtiza <Irtizakh@gmail.com>
2021-05-27 11:09:53 +0500

    Day3 - Operators , switch , range.

---
+++ Day3/MyPlayground.playground/Contents.swift
@@ -0,0 +1,43 @@
+
+
+var str = "Hello, playground"
+
+// Joing 2 arrays with plues operator
+
+let array1 = ["John","Corner"]
+let array2 = ["Alexender" , "Imran"]
+
+let array3 = array1+array2
+
+// Switch
+
+let weather = "sunny"
+
+switch weather {
+case "rain":
+    print("Bring an umbrella")
+case "snow":
+    print("Wrap up warm")
+case "sunny":
+    print("Wear sunscreen")
+    fallthrough
+default:
+    print("Enjoy your day!")
+}
+
+
+// Range
+/*
+ 
+ For example, the range 1..<5 contains the numbers 1, 2, 3, and 4, whereas the range 1...5 contains the numbers 1, 2, 3, 4, and 5.
+ */
+let score = 85
+
+switch score {
+case 0..<50:
+    print("You failed badly.")
+case 50..<85:
+    print("You did OK.")
+default:
+    print("You did great!")
+}
